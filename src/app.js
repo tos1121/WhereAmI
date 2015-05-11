bb9e044a4a3822b3741ed74fc4d37b211d73bcd3
tos1121 <tos1121@gmail.com>
2015-05-11 09:47:10 +0900

    ??

--- src/app.js
+++ src/app.js
@@ -5,11 +5,15 @@
  */
 
 var UI = require('ui');
+//var Vector2 = require('vector2');
 var ajax = require('ajax');
+
+
 
 var main = new UI.Card({
   title: 'Where AM I?',
-	icon: 'images/logo.png',
+//  icon: 'images/menu_icon.png',
+	icon: 'images/h1_header_logo.png',
 	body: 'To get current location push middle button.'
 });
 
@@ -21,12 +25,11 @@ main.on('click', 'up', function(e) {
   var menu = new UI.Menu({
     sections: [{
       items: [{
-        title: 'First Item',
-        icon: 'images/menu_icon.png',
+        title: 'Where Am I?',
+//        icon: 'images/menu_icon.png',
         subtitle: 'Can do Menus'
       }, {
         title: 'Second Item',
-        icon: 'images/menu_icon.png',
         subtitle: 'Subtitle Text'
       }]
     }]
@@ -34,23 +37,25 @@ main.on('click', 'up', function(e) {
 
 
   menu.on('select', function(e) {
-		var card = new UI.Card();
-    card.title(e.item.title);
-    card.body('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+		var wind = new UI.Window();
+
+
+    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+    console.log('The item is titled "' + e.item.title + '"');
   });
   menu.show();
 });
 
 main.on('click', 'select', function(e) {
-  navigator.geolocation.getCurrentPosition(function(pos) {
-    var coords = pos.coords;
-    var geoUrl = 'http://maps.googleapis.com/maps/api/geocode/json?' +
-			'latlng=' + coords.latitude + ',' + coords.longitude + '&sensor=true_or_false';
-    ajax({ url: geoUrl, type: 'json' }, function(data) {
-	    var first = data.results[0];
-		  var formattedAddress = first.formatted_address;
-		  main.body(formattedAddress);
-    });
+	navigator.geolocation.getCurrentPosition(function(pos) {
+		var coords = pos.coords;
+		var geoUrl = 'http://maps.googleapis.com/maps/api/geocode/json?' +
+				'latlng=' + coords.latitude + ',' + coords.longitude + '&sensor=true_or_false';
+		ajax({ url: geoUrl, type: 'json' }, function(data) {
+			var first = data.results[0];
+			var formattedAddress = first.formatted_address;
+			main.body(formattedAddress);
+		});
 	});
 });
 
