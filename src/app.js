2e0751ec612ad7b58727f36a8df263076af30e29
tos1121 <tos1121@gmail.com>
2015-05-11 09:09:15 +0900

    Investigation to put from GIT.

--- src/app.js
+++ src/app.js
@@ -5,16 +5,12 @@
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
 var ajax = require('ajax');
 
-
-
 var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+  title: 'Where AM I?',
+	icon: 'images/logo.png',
+	body: 'To get current location push middle button.'
 });
 
 main.show();
@@ -25,11 +21,12 @@ main.on('click', 'up', function(e) {
   var menu = new UI.Menu({
     sections: [{
       items: [{
-        title: 'Pebble.js',
-//        icon: 'images/menu_icon.png',
+        title: 'First Item',
+        icon: 'images/menu_icon.png',
         subtitle: 'Can do Menus'
       }, {
         title: 'Second Item',
+        icon: 'images/menu_icon.png',
         subtitle: 'Subtitle Text'
       }]
     }]
@@ -37,25 +34,23 @@ main.on('click', 'up', function(e) {
 
 
   menu.on('select', function(e) {
-		var wind = new UI.Window();
-
-
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
+		var card = new UI.Card();
+    card.title(e.item.title);
+    card.body('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
   });
   menu.show();
 });
 
 main.on('click', 'select', function(e) {
-	navigator.geolocation.getCurrentPosition(function(pos) {
-		var coords = pos.coords;
-		var geoUrl = 'http://maps.googleapis.com/maps/api/geocode/json?' +
-				'latlng=' + coords.latitude + ',' + coords.longitude + '&sensor=true_or_false';
-		ajax({ url: geoUrl, type: 'json' }, function(data) {
-			var first = data.results[0];
-			var formattedAddress = first.formatted_address;
-			main.body(formattedAddress);
-		});
+  navigator.geolocation.getCurrentPosition(function(pos) {
+    var coords = pos.coords;
+    var geoUrl = 'http://maps.googleapis.com/maps/api/geocode/json?' +
+			'latlng=' + coords.latitude + ',' + coords.longitude + '&sensor=true_or_false';
+    ajax({ url: geoUrl, type: 'json' }, function(data) {
+	    var first = data.results[0];
+		  var formattedAddress = first.formatted_address;
+		  main.body(formattedAddress);
+    });
 	});
 });
 
