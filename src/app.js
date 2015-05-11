6bfdc9b84e811902ba1d0ebe598b3869ea97effc
tos1121 <tos1121@gmail.com>
2015-05-12 08:58:22 +0900

    Remove unused features

--- src/app.js
+++ src/app.js
@@ -1,49 +1,14 @@
 /**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
+ * Where Am I?
+ * Get the address of you!
  */
 
 var UI = require('ui');
 var ajax = require('ajax');
 
+var main = new UI.Card({ title: 'Where AM I?' });
 
-
-var main = new UI.Card({
-  title: 'Where AM I?',
-  icon: 'images/logo.png',
-	body: 'To get current location push middle button.'
-});
-
-main.show();
-
-
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Where Am I?',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-
-
-  menu.on('select', function(e) {
-    var card = new UI.Card();
-    card.title('The item is titled "' + e.item.title + '"');
-    card.body('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    card.show();
-  });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
+main.on('show', function(e){
 	navigator.geolocation.getCurrentPosition(function(pos) {
 		var coords = pos.coords;
 		var geoUrl = 'http://maps.googleapis.com/maps/api/geocode/json?' +
@@ -53,13 +18,7 @@ main.on('click', 'select', function(e) {
 			var formattedAddress = first.formatted_address;
 			main.body(formattedAddress);
 		});
-	});
+	});	
 });
+main.show();
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
